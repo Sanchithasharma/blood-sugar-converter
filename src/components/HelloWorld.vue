2c0e0ee0dd3022e27d825a992b7e6795a59aa2c7
Sanchithasharma <sanchitha.s.r@gmail.com>
2023-10-08 09:56:54 +1000

    created folder and project

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+defineProps<{ msg: string }>()
+
+const count = ref(0)
+</script>
+
+<template>
+  <div>
+    <h1>
+      Blood Sugar converter
+    </h1>
+  </div>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>

dd6b8729a7f662e1fae0a37d12409d478af58e74
Sanchithasharma <sanchitha.s.r@gmail.com>
2023-10-08 13:36:23 +1000

    add3ed basic wireframke

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,21 +1,49 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref } from "vue";
 
-defineProps<{ msg: string }>()
+defineProps<{ msg: string }>();
 
-const count = ref(0)
+const count = ref(0);
 </script>
 
 <template>
   <div>
-    <h1>
-      Blood Sugar converter
-    </h1>
+    <h1>Blood Sugar converter</h1>
+    <div class="converter-container">
+      <div class="converter-input-container">
+        <label htmlFor="mmol">Blood sugar level in mmol/l</label>
+        <input class="input-element" id="mmol" />
+      </div>
+
+      =
+
+      <div class="converter-input-container">
+        <label htmlFor="mgdl">Blood sugar level in mg/dl</label>
+        <input class="input-element" id="mgdl" />
+      </div>
+    </div>
   </div>
 </template>
 
 <style scoped>
 .read-the-docs {
   color: #888;
+}
+
+.converter-container {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+.converter-input-container {
+  display: flex;
+  flex-direction: column;
+  gap: 5px;
+}
+
+.input-element {
+  height: 30px;
+  font-size: 20px;
+  text-align: center;
 }
 </style>
